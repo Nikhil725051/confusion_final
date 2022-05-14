1284a7f7c2903e169bfa3c93ae2fef565283558c
Nikhil <nikhil725051>
2022-05-14 12:22:20 +0530

    Single Page Applications Part 1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { Card, CardBody, CardImg, CardSubtitle, CardText, CardTitle } from 'reactstrap';
 
 function Home(props) {
     return(
       <div className="container">
-        <h4>Home</h4>
+        <div className="row">
+          <div className="col-md col-12 m-1">
+            <RenderCard item={props.dish}></RenderCard>
+          </div>
+          <div className="col-md col-12 m-1">
+            <RenderCard item={props.promotion}></RenderCard>
+          </div>
+          <div className="col-md col-12 m-1">
+            <RenderCard item={props.leader}></RenderCard>
+          </div>
+        </div>
       </div>
     );
+}
+
+function RenderCard({item}){
+ return (
+  <Card>
+  <CardImg src={item.image} alt={item.name}></CardImg>
+  <CardBody>
+    <CardTitle>{item.name}</CardTitle>
+    {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+    <CardText>{item.description}</CardText>
+  </CardBody>
+</Card>
+ );
 }
 
 export default Home;   
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,7 +5,9 @@ import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import {Routes, Route, Navigate} from 'react-router-dom';
-import {Navbar} from "reactstrap";
+import Contact from "./ContactComponent";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 
 class Main extends Component
 {
@@ -14,6 +16,9 @@ class Main extends Component
     super(props);
     this.state={
       dishes:DISHES,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
+
   };
   }
 
@@ -22,8 +27,9 @@ class Main extends Component
     return (<div>
       <Header></Header>
       <Routes>
-          <Route path='/home' element={<Home></Home>} />
+          <Route path="/home" element={<Home dish={this.state.dishes.filter((dish) => dish.featured)[0]} promotion={this.state.promotions.filter((promo) => promo.featured)[0]}  leader={this.state.leaders.filter((leader) => leader.featured)[0]}></Home>}></Route>
           <Route exact path='/menu' element={<Menu dishes={this.state.dishes}></Menu>} />
+          <Route exact path="/contactus" element={<Contact></Contact>}></Route>
           <Route path="*" element={<Navigate to="/home" replace />} />
       </Routes>
       <Footer></Footer>
